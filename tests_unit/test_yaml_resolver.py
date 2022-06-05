26585ea155954df30606e39dd0295891c1ef1f0c
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-06-05 19:29:55 +0200

    Update test_yaml_resolver.py

--- tests_unit/test_yaml_resolver.py
+++ tests_unit/test_yaml_resolver.py
@@ -2,23 +2,4 @@ import unittest
 import kappaconfig as kc
 
 class TestYamlResolver(unittest.TestCase):
-    def test_to_dotlist(self):
-        input_dict = dict(
-            some_string="some_value",
-            some_list=["a", "b", "c", 5],
-            some_dict=dict(
-                some_nested_dict=dict(
-                    some_nested_dict_value=5
-                )
-            ),
-        )
-        expected = [
-            "some_string=some_value",
-            "some_list[0]=a",
-            "some_list[1]=b",
-            "some_list[2]=c",
-            "some_list[3]=5",
-            "some_dict.some_nested_dict.some_nested_dict_value=5",
-        ]
-        actual = kc.YamlResolver().to_dotlist(input_dict)
-        self.assertEqual(expected, actual)
+    pass
