63c3e913503b7735c88382bf89d67b10a99fcd04
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-06-06 13:52:30 +0200

    Update test_load.py

--- tests_unit/functional/test_load.py
+++ tests_unit/functional/test_load.py
@@ -3,8 +3,9 @@ import kappaconfig.functional.load as load
 from kappaconfig.resolver import Resolver
 
 class TestLoad(unittest.TestCase):
-    def test_from_file_uri(self):
-        expected = dict(
+    @staticmethod
+    def _basic_yaml_dict():
+        return dict(
             some_string="some_value",
             some_int=5,
             some_double=5.,
@@ -15,21 +16,14 @@ class TestLoad(unittest.TestCase):
                 )
             ),
         )
+
+    def test_from_file_uri(self):
+        expected = self._basic_yaml_dict()
         actual = Resolver().resolve(load.from_file_uri("res/basic.yaml"))
         self.assertEqual(expected, actual)
 
     def test_from_string(self):
-        expected = dict(
-            some_string="some_value",
-            some_int=5,
-            some_double=5.,
-            some_list=["a", "b", "c", 5],
-            some_dict=dict(
-                some_nested_dict=dict(
-                    some_nested_dict_value=5
-                )
-            ),
-        )
+        expected = self._basic_yaml_dict()
         with open("res/basic.yaml") as f:
             input_ = f.read()
         actual = Resolver().resolve(load.from_string(input_))
