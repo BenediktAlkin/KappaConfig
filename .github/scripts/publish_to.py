f45239adca775c09d0a7235b6062f388372adf55
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-06-07 21:26:43 +0200

    dev/prod publish improvements

--- .github/scripts/publish_to.py
+++ .github/scripts/publish_to.py
@@ -7,15 +7,14 @@ def get_tagname():
     return args.tagname
 
 tagname = get_tagname()
-# check if tag is valid
 assert tagname[0] == "v", "tagname must start with 'v'"
 version_numbers = tagname[1:].split(".")
 version_number_count = len(version_numbers)
-assert version_number_count == 3 or version_number_count == 4, \
-    "version must contain 3 (for prod publish) or 4 (for test publish) numbers"
-
 if version_number_count == 3:
-    publish_type = "prod"
+    print(f"::set-output name=publish_to_prod::true")
+    print(f"::set-output name=publish_to_dev::true")
+elif version_number_count == 4:
+    print(f"::set-output name=publish_to_prod::false")
+    print(f"::set-output name=publish_to_dev::true")
 else:
-    publish_type = "dev"
-print(f"::set-output name=publish_type::{publish_type}")
+    raise ValueError("version must contain 3 (for prod publish) or 4 (for test publish) numbers")
