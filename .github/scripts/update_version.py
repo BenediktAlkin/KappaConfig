6b78acd2e4e76f2ab9f1edc1ac92b1447d6fdbbf
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-06-09 23:25:02 +0200

    Update update_version.py

--- .github/scripts/update_version.py
+++ .github/scripts/update_version.py
@@ -23,7 +23,7 @@ print("inserting version into kappaconfig/__init__.py")
 with open("kappaconfig/__init__.py", "r") as f:
     content = f.read()
 with open("kappaconfig/__init__.py", "w") as f:
-    f.write(f"__version__ = {get_tagname()}\n\n" + content)
+    f.write(f"__version__ = \"{get_tagname()}\"\n\n" + content)
 
 with open("setup.cfg", "w") as f:
     f.write("\n".join(lines))
