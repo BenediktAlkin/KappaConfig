683c41bd5e2dc0e4c4263ce9e074c78e145bc5ec
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-06-16 20:07:22 +0200

    Update __init__.py

--- kappaconfig/__init__.py
+++ kappaconfig/__init__.py
@@ -5,6 +5,6 @@ from .resolvers.collection_resolvers.collection_resolver import CollectionResolv
 from .resolvers.processors.processor import Processor
 
 from .functional.load import from_string, from_file_uri, from_cli
-from .functional.util import merge, mask_in, mask_out
+from .functional.util import merge, mask_in, mask_out, apply
 from .functional.dotlist import from_dotlist, to_dotlist
 from .functional.convert import from_primitive, to_primitive
